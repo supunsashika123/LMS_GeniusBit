2faaf9b16496cf0488a28e271f6d905df2dd471c
LasithaPBSS <mlasitharandika@gmail.com>
2021-08-23 21:02:27 +0530

    Notificaiton

---
+++ layouts/authentication.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <notifications group="foo" />
+    <Nuxt />
+  </div>
+</template>
+<script>
+export default {
+  components: {},
+}
+</script>
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,45 +1,40 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'GeniusBit',
+    title: "GeniusBit",
     htmlAttrs: {
-      lang: 'en'
+      lang: "en",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" },
+      { name: "format-detection", content: "telephone=no" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
+  css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/vee-validate.js',
+    "~/plugins/vee-validate.js",
+    { src: "~/plugins/notifications-ssr", mode: "server" },
+    { src: "~/plugins/notifications-client", mode: "client" },
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/eslint-module',
-  ],
+  buildModules: ["@nuxtjs/tailwindcss", "@nuxtjs/eslint-module"],
 
   // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
+  modules: [],
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-    transpile: ['vee-validate/dist/rules']
-  }
-}
+    transpile: ["vee-validate/dist/rules"],
+  },
+};
