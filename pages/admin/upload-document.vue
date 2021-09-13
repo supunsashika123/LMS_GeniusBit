acb0ad34cf529fa2f46abac8bf71ee4cc8361f99
LasithaPBSS <mlasitharandika@gmail.com>
2021-09-13 21:54:33 +0530

    upload-document

--- pages/admin/upload-document.vue
+++ pages/admin/upload-document.vue
@@ -106,7 +106,242 @@
   </div>
 </template>
 
+<script>
+import PrimaryButton from '@/components/buttons/PrimaryButton'
+import Multiselect from 'vue-multiselect'
+import { ValidationObserver } from 'vee-validate'
+import TextField from '@/components/TextField'
+import moment from 'moment'
 
+export default {
+  name: 'UploadDocument',
 
+  components: {
+    TextField,
+    PrimaryButton,
+    Multiselect,
+    ValidationObserver,
+  },
+
+  layout: 'admin',
+
+  data() {
+    return {
+      id: this.$route.query.id ?? '',
+      selectedImage: '',
+      classesList: [],
+      title: '',
+      selectedClasses: [],
+      expiry_date: new Date().toISOString(),
+      auto_publish_date: new Date().toISOString(),
+      pdf: '',
+      loading: false,
+    }
+  },
+
+  created() {
+    this.fetchClasses()
+  },
+
+  methods: {
+    capitalizeFirstLetter(text) {
+      return text.charAt(0).toUpperCase() + text.slice(1)
+    },
+
+    handleDeleteButtonClick() {
+      this.$confirm({
+        message: `Are you sure want to delete this file?`,
+        button: {
+          no: 'No',
+          yes: 'Delete',
+        },
+        callback: (confirm) => {
+          if (confirm) {
+            this.deleteDocument()
+          }
+        },
+      })
+    },
+
+    async deleteDocument() {
+      try {
+        this.loading = true
+        await this.$axios.put('doc/' + this.id, {
+          classes: this.selectedClasses.map((c) => c.id),
+          deleted: true,
+        })
+        this.showNotification('success', 'File deleted successfully!')
+        return this.$router.push({
+          path: 'documents',
+        })
+      } catch (e) {
+        this.loading = false
+        this.showNotification('error', 'File delete error!')
+      }
+    },
+
+    async fetchDocument() {
+      this.loading = true
+      const { data } = await this.$axios.get('doc/' + this.id)
+      this.loading = false
+
+      this.title = data.data.title
+      this.expiry_date = data.data.expiry_date
+      this.auto_publish_date = data.data.auto_publish_date
+
+      const tmp = []
+      data.data.classes.forEach((c) => {
+        tmp.push(this.classesList.filter((old) => old.id === c)[0])
+      })
+
+      this.selectedClasses = tmp
+    },
+
+    showNotification(type, text) {
+      this.$notify({
+        type,
+        group: 'foo',
+        title: type === 'success' ? 'Success' : 'Error!',
+        text,
+        position: 'top right',
+      })
+    },
+
+    handleDocumentSelect(evt) {
+      const files = evt.target.files
+      this.pdf = files[0]
+    },
+
+    async fetchClasses() {
+      try {
+        this.loading = true
+        const { data } = await this.$axios.get('class')
+        this.loading = false
+        if (data) {
+          this.classesList = data.data
+          if (this.id) await this.fetchDocument()
+        }
+      } catch (e) {}
+    },
+
+    async resetFields() {
+      this.title = ''
+      this.selectedClasses = []
+      this.selectedImage = ''
+      this.expiry_date = ''
+      this.auto_publish_date = ''
+
+      await this.$refs.documentSubmitForm.reset()
+    },
+
+    async handleDocumentUploadClick() {
+      if (!(await this.$refs.documentSubmitForm.validate())) {
+        this.showNotification('error', 'Please fill required fields.')
+        return false
+      }
+
+      if (!this.title) {
+        this.showNotification('error', 'Please add title!')
+        return false
+      }
+
+      if (!this.selectedClasses.length) {
+        this.showNotification('error', 'Please select classes!')
+        return false
+      }
+
+      if (!this.expiry_date) {
+        this.showNotification('error', 'Please select expiry date!')
+        return false
+      }
+
+      if (!this.auto_publish_date) {
+        this.showNotification('error', 'Please select publish date!')
+        return false
+      }
+
+      const publishDate = new Date(this.auto_publish_date)
+      const expiryDate = new Date(this.expiry_date)
+
+      if (expiryDate < publishDate) {
+        this.showNotification(
+          'error',
+          'Expiration date is greater than publish date!'
+        )
+        return false
+      }
+
+      if (!this.id) {
+        await this.postDocument()
+      } else {
+        await this.updateDocument()
+      }
+    },
+
+    async postDocument() {
+      if (!this.pdf) {
+        this.showNotification('error', 'Please upload file!')
+        return false
+      }
+
+      try {
+        const formData = new FormData()
+        formData.append('file', this.pdf)
+        formData.append(
+          'payload',
+          JSON.stringify({
+            title: this.title,
+            classes: this.selectedClasses.map((c) => c.id),
+            expiry_date: moment.utc(this.expiry_date),
+            auto_publish_date: moment.utc(this.auto_publish_date),
+          })
+        )
+
+        this.loading = true
+        await this.$axios.post('doc', formData, {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        })
+
+        this.loading = false
+
+        this.showNotification('success', 'Document submitted successfully!')
+
+        await this.resetFields()
+      } catch (e) {
+        this.showNotification(
+          'error',
+          'Something went wrong when uploading doc!'
+        )
+      }
+    },
+
+    async updateDocument() {
+      try {
+        this.loading = true
+        await this.$axios.put('doc/' + this.id, {
+          title: this.title,
+          classes: this.selectedClasses.map((c) => c.id),
+          expiry_date: this.expiry_date,
+          auto_publish_date: this.auto_publish_date,
+          url: this.url,
+          deleted: false,
+        })
+        this.loading = false
+
+        this.showNotification('success', 'Document submitted successfully!')
+
+        await this.resetFields()
+      } catch (e) {
+        this.showNotification(
+          'error',
+          'Something went wrong when uploading doc!'
+        )
+      }
+    },
+  },
+}
+</script>
 
 <style scoped></style>
